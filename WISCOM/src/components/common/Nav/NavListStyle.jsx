dc1042bb895ac2ada8707107bb81ab9e7e913f9c
harim061 <doris5093@duksung.ac.kr>
2023-09-28 20:37:11 +0900

    feature : 테블릿, 폰 버전
    나브바 애니메이션 생성, 반응형 제작
    
    fixes #1

---
+++ WISCOM/src/components/common/Nav/NavListStyle.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MenuComponente = styled.div`
+  width: 320px;
+  height: 568px;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 0px 12px rgba(0, 0, 0, 0.1), 0 0 1px 1px rgba(0, 0, 0, 0.1), inset 0 0 0 12px rgba(255, 255, 255, 1);
+  overflow: hidden;
+  position: relative;
+  background-color: #eaecef;
+  margin: 30px auto;
+`;
--- WISCOM/src/components/common/Nav/NavStyle.jsx
+++ WISCOM/src/components/common/Nav/NavStyle.jsx
@@ -35,8 +35,34 @@ export const NavTag = styled.a`
   &:hover {
     color: white;
   }
+
+  @media screen and (max-width: 900px) {
+    display: none;
+  }
 `;
 
+export const NavBtn = styled.div`
+  width: 150px;
+  height: 45px;
+  border-radius: 50px;
+  background: #75ff72;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+  color: black;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+  text-align: center;
+
+  &:hover {
+    color: white;
+  }
+  @media screen and (min-width: 900px) {
+    display: none;
+  }
+`;
 export const LogoImg = styled.div`
   background-image: url(${Logo});
   width: 320px;
@@ -45,10 +71,27 @@ export const LogoImg = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+
+  @media screen and (max-width: 500px) {
+    width: 250px;
+    height: 150px;
+  }
+  @media screen and (max-width: 300px) {
+    width: 200px;
+  }
 `;
 
 export const Text = styled.div`
   text-align: center;
-  font-size: 48px;
+  font-size: 45px;
   font-weight: 700;
+
+  @media screen and (max-width: 500px) {
+    font-size: 35px;
+  }
+  @media screen and (max-width: 300px) {
+    font-size: 26px;
+  }
 `;
