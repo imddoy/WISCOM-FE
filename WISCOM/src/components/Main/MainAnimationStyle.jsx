176952c223df387a571e66ec1eceab22d77fb519
imddoy <imddoy@duksung.ac.kr>
2023-11-03 02:21:24 +0900

    Feat: 메인 애니메이션 부드럽게 수정

--- WISCOM/src/components/Main/MainAnimationStyle.jsx
+++ WISCOM/src/components/Main/MainAnimationStyle.jsx
@@ -10,6 +10,7 @@ export const drop = keyframes`
 75%{
   left: 40vw;
   bottom: 50vw;
+  opacity: 1;
 `;
 export const fadein = keyframes`
 0%,50% {
@@ -37,9 +38,9 @@ export const Text = styled.p`
   font-weight: 500;
   line-height: normal;
   position: absolute;
-  bottom: 45vw;
+  bottom: 40vw;
   left: 10vw;
-  animation: ${fadein} 4s ease-in-out;
+  animation: ${fadein} 7s ease-in-out;
 `;
 
 export const Leading = styled.p`
@@ -53,7 +54,7 @@ export const Leading = styled.p`
   transform: rotate(90deg);
   bottom: 15vw;
   left: -5vw;
-  animation: ${drop} 2s ease-out;
+  animation: ${drop} 3s ease-out;
 `;
 export const Woman = styled.p`
   color: ${(props) => props.theme.colors.white};
@@ -67,7 +68,7 @@ export const Woman = styled.p`
   transform: rotate(-25deg);
   left: 6vw;
   bottom: 12vw;
-  animation: ${drop} 1.6s ease-out;
+  animation: ${drop} 2.6s ease-out;
 `;
 export const Achieving = styled.p`
   color: ${(props) => props.theme.colors.white};
@@ -79,7 +80,7 @@ export const Achieving = styled.p`
   line-height: normal;
   position: absolute;
   bottom: 4vw;
-  animation: ${drop} 1.6s ease-out;
+  animation: ${drop} 2.6s ease-out;
 `;
 export const Developer = styled.p`
   color: var(--LIGHTGREEN, #75ff72);
@@ -91,7 +92,7 @@ export const Developer = styled.p`
   line-height: normal;
   position: absolute;
   bottom: 0;
-  animation: ${drop} 1.6s ease-out;
+  animation: ${drop} 2.6s ease-out;
 `;
 export const Respect = styled.p`
   color: ${(props) => props.theme.colors.white};
@@ -104,8 +105,8 @@ export const Respect = styled.p`
   position: absolute;
   transform: rotate(-70deg);
   bottom: 29vw;
-  right: 28vw;
-  animation: ${drop} 1.9s ease-out;
+  left: 53vw;
+  animation: ${drop} 2.9s ease-out;
 `;
 export const Perfect = styled.p`
   color: ${(props) => props.theme.colors.white};
@@ -118,43 +119,43 @@ export const Perfect = styled.p`
   position: absolute;
   transform: rotate(-40deg);
   bottom: 6vw;
-  right: 0;
-  animation: ${drop} 1.6s ease-out;
+  left: 82vw;
+  animation: ${drop} 2.6s ease-out;
 `;
 export const WebImg = styled.img`
   width: 20vw;
   position: absolute;
   bottom: 22vw;
   left: 38vw;
-  animation: ${drop} 1.9s ease-out;
+  animation: ${drop} 2.9s ease-out;
 `;
 export const IosImg = styled.img`
   width: 14.8vw;
   position: absolute;
   bottom: 3vw;
   left: 22.5vw;
-  animation: ${drop} 1.7s ease-out;
+  animation: ${drop} 2.7s ease-out;
 `;
 export const UiImg = styled.img`
   width: 20vw;
   position: absolute;
   bottom: 0;
   left: 31vw;
-  animation: ${drop} 1.6s ease-out;
+  animation: ${drop} 2.6s ease-out;
 `;
 export const DsceImg = styled.img`
   width: 22vw;
   position: absolute;
   left: 8.5vw;
   bottom: 22vw;
-  animation: ${drop} 1.8s linear;
+  animation: ${drop} 2.85s linear;
 `;
 export const MetaverseImg = styled.img`
   width: 22vw;
   position: absolute;
   bottom: 8vw;
-  right: 28vw;
-  animation: ${drop} 1.7s ease-out;
+  left: 49vw;
+  animation: ${drop} 2.7s ease-out;
 `;
 export const AppImg = styled.img`
   width: 20vw;
@@ -162,34 +163,34 @@ export const AppImg = styled.img`
   bottom: 0;
   left: 46vw;
   margin-bottom: 1vw;
-  animation: ${drop} 1.3s ease-out;
+  animation: ${drop} 2.3s ease-out;
 `;
 export const AiImg = styled.img`
   width: 20vw;
   position: absolute;
   bottom: 1vw;
-  right: 16vw;
-  animation: ${drop} 1.8s ease-out;
+  left: 63vw;
+  animation: ${drop} 2.8s ease-out;
 `;
 export const GameImg = styled.img`
   width: 20vw;
   position: absolute;
   bottom: 9.5vw;
-  right: 4vw;
-  animation: ${drop} 1.7s ease-out;
+  left: 75vw;
+  animation: ${drop} 2.7s ease-out;
 `;
 export const Star1 = styled.img`
   width: 3.5vw;
   position: absolute;
   bottom: 18vw;
-  right: 46vw;
-  animation: ${drop} 1.8s ease-out;
+  left: 50vw;
+  animation: ${drop} 2.8s ease-out;
 `;
 export const Star2 = styled.img`
   width: 3.5vw;
   position: absolute;
   bottom: 0;
-  right: 27vw;
+  left: 65vw;
   margin: 2vw;
-  animation: ${drop} 1.6s ease-out;
+  animation: ${drop} 2.6s ease-out;
 `;
