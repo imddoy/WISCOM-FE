bc62f6e1694bea1558a157eadef809ebce070c52
imddoy <imddoy@duksung.ac.kr>
2023-11-06 20:19:12 +0900

    Design: 챗봇 퍼블리싱

---
+++ WISCOM/src/components/Chatbot/ChatStyle.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const AnswerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const AnswerBtn = styled.button`
+  width: auto-fit;
+  height: 30px;
+  border-radius: 50px;
+  padding: 0 10px 0 10px;
+  border: 1px solid #75ff72;
+  font-size: 10px;
+`;
